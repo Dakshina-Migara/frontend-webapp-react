8b2836a7163d58b82eb9a4934b48b670326d73ec
Dakshina-Migara <dakshinamigara666@gmail.com>
2025-12-15 13:17:25 +0530

    create the logo

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Imgnav from '../common/component/Imgnav/Imgnav'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>hi</h1>
+      <Imgnav />
     </>
   )
 }
---
+++ src/common/component/Imgnav/Imgnav.jsx
@@ -0,0 +1,10 @@
+import './Imgnav.css';
+import * as React from 'react';
+
+export default function Imgnav() {
+    return (
+        <>
+            <img src="src\assets\6dab13ff1a3a71ef60c41a9a405cb23f7e60d290.png" alt="image" className="logonav" />
+        </>
+    );
+}
