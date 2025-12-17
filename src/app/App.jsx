3ebae946560f7dbbf918457efded5aa80f8fbe89
Dakshina-Migara <dakshinamigara666@gmail.com>
2025-12-17 12:19:23 +0530

    create the home page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,27 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import Imgnav from '../common/component/Imgnav/Imgnav'
-import ButtonNav from '../common/component/ButtonNav/buttonNav'
-import Footer from '../common/component/Footer/Footer'
-import ProcessBar from '../common/component/ProcessBar/ProcessBar'
-import TextArea from '../common/component/TextArea/TextArea'
-import GenDropdown from '../common/component/GenDropdown/GenDropdown'
-import StateSwitch from '../common/component/StateSwitch/StateSwitch'
-import ChildSlider from '../common/component/ChildSlider/ChildSlider'
+import HomePage from '../page/HomePage/HomePage.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Imgnav />
-      <ButtonNav />
-      <Footer />
-      <ProcessBar />
-      <TextArea />
-      <GenDropdown />
-      <StateSwitch />
-      <ChildSlider />
+      <HomePage />
     </>
   )
 }
