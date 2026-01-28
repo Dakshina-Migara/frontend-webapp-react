ee202860e8c922568fb4c29e42312e09a50a9330
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-01-28 21:15:25 +0530

    update the slider

--- src/common/component/ChildSlider/ChildSlider.jsx
+++ src/common/component/ChildSlider/ChildSlider.jsx
@@ -4,25 +4,35 @@ import Slider from '@mui/material/Slider';
 function valuetext(value) {
     return `${value}°C`;
 }
-export default function ChildSlider({ onChange, value, marginleft, marginTop }) {
+
+export default function ChildSlider({ onChange, value }) {
     return (
-        <Box sx={{ width: 250, marginLeft: marginleft, marginTop: marginTop }}>
+        <Box
+            sx={{
+                width: '100%',
+                maxWidth: { xs: 190, sm: 220, md: 280, lg: 320 },
+                mx: 'auto',
+                mt: 2,
+                px: 1,
+            }}
+        >
             <Slider
                 aria-label="ChildCount"
                 value={value}
                 onChange={onChange}
                 getAriaValueText={valuetext}
                 valueLabelDisplay="on"
-                shiftStep={10}
                 step={1}
                 marks
                 min={0}
                 max={10}
                 sx={{
                     color: '#FE5000',
-                    '& .MuiSlider-markActive': { bgcolor: '#FE5000' },
+                    '& .MuiSlider-markActive': {
+                        bgcolor: '#FE5000',
+                    },
                 }}
             />
         </Box>
-    )
+    );
 }
